13ad7222b29d51a4833183011de98eca60bd17a9
SvetlanaTsim <svetline87@mail.ru>
2022-02-23 00:03:03 +1000

    added lesson_1 authors

--- library/settings.py
+++ library/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authors',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -103,11 +105,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Vladivostok'
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
