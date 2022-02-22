3c310bfdc453af3a3b1514e089b42583553a0f3c
SvetlanaTsim <svetline87@mail.ru>
2022-02-23 01:30:43 +1000

    lesson_1 create_users.py fixed

--- users/management/commands/create_users.py
+++ users/management/commands/create_users.py
@@ -8,6 +8,3 @@ class Command(BaseCommand):
         superuser = User.objects.create_superuser('admin', 'admin@geekbrains.ru', '123')
         test_user_1 = User.objects.create_user('user_1', 'user_1@geekbrains.ru', '456')
         test_user_2 = User.objects.create_user('user_2', 'user_2@geekbrains.ru', '789')
-        # for user in users:
-        #     user_profile = ShopUserProfile.objects.create(user=user)
-        #     user_profile.save()
