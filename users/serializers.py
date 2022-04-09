71c15fbaf29797b11f21d132eb58618fdd31698f
SvetlanaTsim <svetline87@mail.ru>
2022-04-09 23:33:55 +1000

    add lesson_9

--- users/serializers.py
+++ users/serializers.py
@@ -8,6 +8,12 @@ class UserModelSerializer(HyperlinkedModelSerializer):
         fields = ['uid', 'username', 'first_name', 'last_name', 'email']
 
 
+class UserModelSerializerExtended(HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['uid', 'username', 'first_name', 'last_name', 'email', 'is_superuser', 'is_staff']
+
+
 class SimpleUserModelSerializer(ModelSerializer):
     class Meta:
         model = User
--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from .models import User
-from .serializers import UserModelSerializer
+from .serializers import UserModelSerializer, UserModelSerializerExtended
 from rest_framework import mixins, viewsets
 
 
@@ -9,3 +9,9 @@ class UserModelViewSet(mixins.ListModelMixin, mixins.UpdateModelMixin,
                        mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
+
+    def get_serializer_class(self):
+        if self.request.version == '2.0':
+            return UserModelSerializerExtended
+
+        return UserModelSerializer
